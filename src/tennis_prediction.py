bffa1367c3a0437280f87962f9bfcc814a609f3f
davy-datascience <davy.datascience@gmail.com>
2020-07-03 09:02:50 +0200

    add feature importance

--- src/tennis_prediction.py
+++ src/tennis_prediction.py
@@ -1,63 +1,52 @@
 import pandas as pd
 import numpy as np
-
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_absolute_error
+
+def inverseHalfDataset(dataset_input):
+    '''inverse 50% of the dataset - for option 2'''
+    inversed_dataset = pd.DataFrame()
+    inversed_dataset["player_1_points"] = np.where(dataset_input.index % 2 == 0, dataset_input["player_1_points"] , dataset_input["player_2_points"])
+    inversed_dataset["player_2_points"] = np.where(dataset_input.index % 2 == 0, dataset_input["player_2_points"] , dataset_input["player_1_points"])
+    inversed_dataset["player_1_wins"] = np.where(dataset_input.index % 2 == 0, 1, 0)
+    return inversed_dataset    
 
 # Read the data
 list_datasets = []
-for year in range(2000, 2010):
+for year in range(2010, 2020):
     dataset = pd.read_csv("https://raw.githubusercontent.com/davy-datascience/tennis-prediction/master/datasets/atp_matches_{}.csv".format(year))
     list_datasets.append(dataset)
 
 full_dataset = pd.concat(list_datasets)
 
-
-features = ["winner_rank_points", "loser_rank_points", "surface"]
+features = ["winner_rank_points", "loser_rank_points"]
 
 dataset = full_dataset[features]
 
+#drop rows with null value
+dataset = dataset.dropna()
+
 dataset = dataset.rename(columns={'winner_rank_points': 'player_1_points', 'loser_rank_points': 'player_2_points'})
-dataset["p1Wins"] = 1
+dataset["player_1_wins"] = 1
 
 # Separate the dataset into a training set and a test set
 train, test = train_test_split(dataset, test_size = 0.2)
+train = inverseHalfDataset(train)
 
-def inverseDataset(dataset_input):
-    inversed_dataset = pd.DataFrame()
-    inversed_dataset["player_1_points"] = dataset_input["player_2_points"]
-    inversed_dataset["player_2_points"] = dataset_input["player_1_points"]
-    inversed_dataset["surface"] = dataset_input["surface"]
-    inversed_dataset["p1Wins"] = 0
-    return inversed_dataset
-
-inversed_train = inverseDataset(train)
-train = pd.concat([train, inversed_train])
-inversed_test = inverseDataset(test)
-test = pd.concat([test, inversed_test])
+X_train = train[["player_1_points", "player_2_points"]]
+y_train = train.player_1_wins
+X_test = test[["player_1_points", "player_2_points"]]
+y_test = test.player_1_wins
 
 
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
+my_model = RandomForestClassifier(n_estimators=100, random_state=0)
+my_model.fit(X_train, y_train)
 
-columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop="first"), ["surface"])], remainder='passthrough')
-#remainder='passthrough' : keep other columns (default:'drop')
-
-dataset = np.array(columnTransformer.fit_transform(dataset))
-
-
-
-# Predict the test set with the sklearn algorithm
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
-y_pred2 = regressor.predict(X_test)
-print("MAE for the algorithm of the sklearn module: {}".format(mean_absolute_error(y_pred2, y_test)))
-
-
-
-
-
+# Predict
+y_pred = pd.Series(classifier.predict(X_test), index = y_test.index)
+mae = mean_absolute_error(y_pred, y_test)
+print("MAE using option 2: {}".format(mae))
 
 
 # Ideas : Nationality
@@ -71,3 +60,11 @@ test = dataset[(criteria1 | criteria2 )]
 
 unique = np.unique(np.concatenate([dataset["winner_id"].unique(), dataset["loser_id"].unique()]))
 '''
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop="first"), ["surface"])], remainder='passthrough')
+#remainder='passthrough' : keep other columns (default:'drop')
+
+dataset = np.array(columnTransformer.fit_transform(dataset))
