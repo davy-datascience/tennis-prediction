9436f1f3c9411ebebabf90cc71d14a690a5f5071
davy-datascience <davy.datascience@gmail.com>
2020-07-24 16:12:58 +0200

    WIP

---
+++ src/data_preparation.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import _VectorizerMixin
+from sklearn.feature_selection import SelectorMixin
+import re
+import numba
+from numba import njit
+
+def inverseHalfDataset(dataset):
+    '''inverse 50% of the dataset - for option 2'''
+    inv = dataset.copy()
+    print(dataset.columns)
+    for col in dataset.columns:
+        if col.startswith("p1") and col != "p1_wins":
+            inv[col] = np.where(dataset.index % 2 == 0, dataset[col] , dataset["p2" + col[2:]])
+        elif col.startswith("p2"):
+            inv[col] = np.where(dataset.index % 2 == 0, dataset[col] , dataset["p1" + col[2:]])
+            
+    inv["p1_wins"] = np.where(dataset.index % 2 == 0, 1, 0)
+    return inv 
+
+def inverseDataset(dataset, cols):
+    '''inverse 50% of the dataset - for option 2'''
+    inv = dataset.copy()
+    for col in cols:
+        inv["p1_" + col] = dataset["p2_" + col]
+        inv["p2_" + col] = dataset["p1_" + col]
+   
+    inv["p1_wins"] = ~dataset["p1_wins"]
+    return inv       
+
+def renameColumnNames(dataset, long_col, short_col):
+    columns = {}
+    for col in long_col:
+        columns["winner_" + col] = "p1_" + col
+        columns["loser_" + col] = "p2_" + col
+    
+    for col in short_col:
+        columns["w_" + col] = "p1_" + col
+        columns["l_" + col] = "p2_" + col
+        
+    dataset.rename(columns=columns, inplace=True)
+    return dataset
+
+def get_feature_out(estimator, feature_in):
+    if hasattr(estimator,'get_feature_names'):
+        if isinstance(estimator, _VectorizerMixin):
+            # handling all vectorizers
+            return [f'vec_{f}' \
+                for f in estimator.get_feature_names()]
+        else:
+            return estimator.get_feature_names(feature_in)
+    elif isinstance(estimator, SelectorMixin):
+        return np.array(feature_in)[estimator.get_support()]
+    else:
+        return feature_in
+
+def get_ct_feature_names(ct):
+    # handles all estimators, pipelines inside ColumnTransfomer
+    # doesn't work when remainder =='passthrough'
+    # which requires the input column names.
+    output_features = []
+
+    for name, estimator, features in ct.transformers_:
+        if name!='remainder':
+            if isinstance(estimator, Pipeline):
+                current_features = features
+                for step in estimator:
+                    current_features = get_feature_out(step, current_features)
+                features_out = current_features
+            else:
+                features_out = get_feature_out(estimator, features)
+            output_features.extend(features_out)
+        elif estimator=='passthrough':
+            output_features.extend(ct._feature_names_in[features])
+                
+    return output_features
+
+
+def extractGames(scores):
+    gamesWon = []
+    for score in scores: 
+        sets = score.split()
+        games5 = [(int(re.search("^([0-7])-([0-7]).*$", s).group(1)), int(re.search("^([0-7])-([0-7]).*$", s).group(2))) for s in sets if re.search("^[0-7]-[0-7].*$", s)]
+        gamesWon.append((sum([game[0] for game in games5]), sum([game[1] for game in games5])))
+    return gamesWon
+
+
+@numba.vectorize
+def addWithNumba(a, b):
+    return a + b
+
+@numba.vectorize
+def divideWithNumba(a, b):
+    return a / b
+
+def getBpSavedRatio(a, b):
+    return 1 if b == 0 else (a/b)
+
+def getPreviousResults(player_results, index, p1_id, p2_id):
+    results_p1 = player_results[p1_id]
+    prev_res_p1 = pd.DataFrame([results_p1.loc[i] for i in results_p1.index if i < index])
+    
+    results_p2 = player_results[p2_id]
+    prev_res_p2 = pd.DataFrame([results_p2.loc[i] for i in results_p2.index if i < index])
+    
+    (
+     p1_ace_ratio_last3, p2_ace_ratio_last3, p1_df_ratio_last3, p2_df_ratio_last3, p1_1stIn_ratio_last3, 
+     p2_1stIn_ratio_last3, p1_1stWon_ratio_last3, p2_1stWon_ratio_last3, p1_2ndWon_ratio_last3, p2_2ndWon_ratio_last3,
+     p1_bpSaved_ratio_last3, p2_bpSaved_ratio_last3, p1_bpFaced_ratio_last3, p2_bpFaced_ratio_last3
+     ) = (None, None, None, None, None, None, None, None, None, None, None, None, None, None)
+    
+    if len(prev_res_p1) > 0 :
+        p1_ace_ratio_last3 = prev_res_p1["p1_ace_ratio"].tail(3).mean()
+        p1_df_ratio_last3 = prev_res_p1["p1_df_ratio"].tail(3).mean()
+        p1_1stIn_ratio_last3 = prev_res_p1["p1_1stIn_ratio"].tail(3).mean()
+        p1_1stWon_ratio_last3 = prev_res_p1["p1_1stWon_ratio"].tail(3).mean()
+        p1_2ndWon_ratio_last3 = prev_res_p1["p1_2ndWon_ratio"].tail(3).mean()
+        p1_bpSaved_ratio_last3 = prev_res_p1["p1_bpSaved_ratio"].tail(3).mean()        
+        p1_bpFaced_ratio_last3 = prev_res_p1["p1_bpFaced"].tail(3).mean()  
+        
+    if len(prev_res_p2) > 0 :
+        p2_ace_ratio_last3 = prev_res_p2["p2_ace_ratio"].tail(3).mean()
+        p2_df_ratio_last3 = prev_res_p2["p2_df_ratio"].tail(3).mean()
+        p2_1stIn_ratio_last3 = prev_res_p2["p2_1stIn_ratio"].tail(3).mean()
+        p2_1stWon_ratio_last3 = prev_res_p2["p2_1stWon_ratio"].tail(3).mean()
+        p2_2ndWon_ratio_last3 = prev_res_p2["p2_2ndWon_ratio"].tail(3).mean()
+        p2_bpSaved_ratio_last3 = prev_res_p2["p2_bpSaved_ratio"].tail(3).mean()
+        p2_bpFaced_ratio_last3 = prev_res_p2["p2_bpFaced"].tail(3).mean()  
+    
+    return (p1_ace_ratio_last3, p2_ace_ratio_last3, p1_df_ratio_last3, p2_df_ratio_last3, 
+            p1_1stIn_ratio_last3, p2_1stIn_ratio_last3, p1_1stWon_ratio_last3, p2_1stWon_ratio_last3, 
+            p1_2ndWon_ratio_last3, p2_2ndWon_ratio_last3, p1_bpSaved_ratio_last3, p2_bpSaved_ratio_last3,
+            p1_bpFaced_ratio_last3, p2_bpFaced_ratio_last3)
--- src/tennis_prediction.py
+++ src/tennis_prediction.py
@@ -1,70 +1,171 @@
 import pandas as pd
 import numpy as np
+from data_preparation import inverseHalfDataset, inverseDataset, renameColumnNames, extractGames, divideWithNumba, getBpSavedRatio, getPreviousResults
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import mean_absolute_error
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
 
-def inverseHalfDataset(dataset_input):
-    '''inverse 50% of the dataset - for option 2'''
-    inversed_dataset = pd.DataFrame()
-    inversed_dataset["player_1_points"] = np.where(dataset_input.index % 2 == 0, dataset_input["player_1_points"] , dataset_input["player_2_points"])
-    inversed_dataset["player_2_points"] = np.where(dataset_input.index % 2 == 0, dataset_input["player_2_points"] , dataset_input["player_1_points"])
-    inversed_dataset["player_1_wins"] = np.where(dataset_input.index % 2 == 0, 1, 0)
-    return inversed_dataset    
+long_col = ["id", "name", "hand", "ht", "ioc", "age", "rank", "rank_points"]
+short_col = ["ace", "df", "svpt", "1stIn", "1stWon", "2ndWon", "SvGms", "bpSaved", "bpFaced"]
 
 # Read the data
 list_datasets = []
-for year in range(2010, 2020):
+for year in range(2016, 2021):
     dataset = pd.read_csv("https://raw.githubusercontent.com/davy-datascience/tennis-prediction/master/datasets/atp_matches_{}.csv".format(year))
     list_datasets.append(dataset)
 
-full_dataset = pd.concat(list_datasets)
+dataset = pd.concat(list_datasets)
+dataset.reset_index(drop=True, inplace=True)
 
-features = ["winner_rank_points", "loser_rank_points"]
-
-dataset = full_dataset[features]
+dataset.drop(columns=["winner_seed", "winner_entry", "loser_seed", "loser_entry"], inplace=True)
 
 #drop rows with null value
-dataset = dataset.dropna()
+dataset.dropna(inplace=True)
 
-dataset = dataset.rename(columns={'winner_rank_points': 'player_1_points', 'loser_rank_points': 'player_2_points'})
-dataset["player_1_wins"] = 1
+
+dataset = renameColumnNames(dataset, long_col, short_col)
+
+dataset["p1_wins"] = True
+
+games = extractGames(dataset['score'])
+dataset["p1_games_won"] = [game[0] for game in games]
+dataset["p2_games_won"] = [game[1] for game in games]
+# TODO CALCULATE GAME WON RATIO
+
+dataset.drop(dataset[(dataset["p1_svpt"] == 0) | (dataset["p2_svpt"] == 0)].index, inplace=True) 
+
+dataset["p1_ace_ratio"] = divideWithNumba(dataset["p1_ace"].to_numpy(), dataset["p1_svpt"].to_numpy())
+dataset["p2_ace_ratio"] = divideWithNumba(dataset["p2_ace"].to_numpy(), dataset["p2_svpt"].to_numpy())
+dataset["p1_df_ratio"] = divideWithNumba(dataset["p1_df"].to_numpy(), dataset["p1_svpt"].to_numpy())
+dataset["p2_df_ratio"] = divideWithNumba(dataset["p2_df"].to_numpy(), dataset["p2_svpt"].to_numpy())
+dataset["p1_1stIn_ratio"] = divideWithNumba(dataset["p1_1stIn"].to_numpy(), dataset["p1_svpt"].to_numpy())
+dataset["p2_1stIn_ratio"] = divideWithNumba(dataset["p2_1stIn"].to_numpy(), dataset["p2_svpt"].to_numpy())
+dataset["p1_1stWon_ratio"] = divideWithNumba(dataset["p1_1stWon"].to_numpy(), dataset["p1_svpt"].to_numpy())
+dataset["p2_1stWon_ratio"] = divideWithNumba(dataset["p2_1stWon"].to_numpy(), dataset["p2_svpt"].to_numpy())
+dataset["p1_2ndWon_ratio"] = divideWithNumba(dataset["p1_2ndWon"].to_numpy(), dataset["p1_svpt"].to_numpy())
+dataset["p2_2ndWon_ratio"] = divideWithNumba(dataset["p2_2ndWon"].to_numpy(), dataset["p2_svpt"].to_numpy())
+
+dataset["p1_bpFaced_ratio"] = divideWithNumba(dataset["p1_bpFaced"].to_numpy(), dataset["p1_svpt"].to_numpy()) # Break points Faced per return-game
+dataset["p2_bpFaced_ratio"] = divideWithNumba(dataset["p2_bpFaced"].to_numpy(), dataset["p2_svpt"].to_numpy()) # Break points Faced per return-game
+dataset["p1_bpSaved_ratio"] = [getBpSavedRatio(row[0], row[1]) for row in dataset[["p1_bpSaved", "p1_bpFaced"]].to_numpy()]       
+dataset["p2_bpSaved_ratio"] = [getBpSavedRatio(row[0], row[1]) for row in dataset[["p2_bpSaved", "p2_bpFaced"]].to_numpy()]       
+dataset['tourney_date'] = pd.to_datetime(dataset['tourney_date'], format="%Y%m%d")    
+
+
+player_ids = np.unique(np.concatenate([dataset["p1_id"].unique(), dataset["p2_id"].unique()]))
+
+
+import time
+
+start_time = time.time()
+player_results = {}
+
+for pid in player_ids:
+    '''idx = np.where((dataset["p1_id"] == pid) | (dataset["p2_id"] == pid))
+    all_matchs = dataset.iloc[idx[0]]'''
+    all_matchs = dataset.loc[(dataset["p1_id"] == pid) | (dataset["p2_id"] == pid)]
+    all_wins = all_matchs[all_matchs["p1_id"] == pid]
+    all_lost = all_matchs[all_matchs["p2_id"] == pid]
+    
+    player_results[pid]= pd.concat([all_wins, inverseDataset(all_lost, long_col + short_col)]).sort_index()
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+start_time = time.time()
+results = [getPreviousResults(player_results, index, ids[0], ids[1]) for index, ids in dataset[["p1_id", "p2_id"]].iterrows()]
+dataset["p1_ace_ratio_last3"] = [result[0] for result in results]
+dataset["p2_ace_ratio_last3"] = [result[1] for result in results]
+dataset["p1_df_ratio_last3"] = [result[2] for result in results]
+dataset["p2_df_ratio_last3"] = [result[3] for result in results]
+dataset["p1_1stIn_ratio_last3"] = [result[4] for result in results]
+dataset["p2_1stIn_ratio_last3"] = [result[5] for result in results]
+dataset["p1_1stWon_ratio_last3"] = [result[6] for result in results]
+dataset["p2_1stWon_ratio_last3"] = [result[7] for result in results]
+dataset["p1_2ndWon_ratio_last3"] = [result[8] for result in results]
+dataset["p2_2ndWon_ratio_last3"] = [result[9] for result in results]
+dataset["p1_bpSaved_ratio_last3"] = [result[10] for result in results]
+dataset["p2_bpSaved_ratio_last3"] = [result[11] for result in results]
+dataset["p1_bpFaced_ratio_last3"] = [result[12] for result in results]
+dataset["p2_bpFaced_ratio_last3"] = [result[13] for result in results]
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+removed_cols = ["tourney_id", "score", "minutes", "p1_ace", "p1_df", "p1_svpt", "p1_1stIn", "p1_1stWon", "p1_2ndWon", "p1_SvGms",
+                "p1_SvGms", "p1_bpSaved", "p1_bpFaced", "p2_ace", "p2_df", "p2_svpt", "p2_1stIn", "p2_1stWon", "p2_2ndWon", 
+                "p2_SvGms", "p2_SvGms", "p2_bpSaved", "p2_bpFaced", "p1_games_won", "p2_games_won", "p1_ace_ratio", "p1_df_ratio", 
+                "p1_1stIn_ratio", "p1_1stWon_ratio", "p1_2ndWon_ratio", "p1_bpSaved_ratio", "p2_ace_ratio", "p2_df_ratio", 
+                "p2_1stIn_ratio", "p2_1stWon_ratio", "p2_2ndWon_ratio", "p2_bpSaved_ratio", "p1_bpFaced_ratio", "p2_bpFaced_ratio",
+                "match_num", "p1_id", "p2_id", "p1_rank", "p2_rank",
+                "tourney_date"]
+dataset.drop(columns=removed_cols, inplace=True)
+
+dataset = inverseHalfDataset(dataset)
+
+X = dataset.drop('p1_wins', axis=1)
+y = dataset["p1_wins"]
 
 # Separate the dataset into a training set and a test set
-train, test = train_test_split(dataset, test_size = 0.2)
-train = inverseHalfDataset(train)
-
-X_train = train[["player_1_points", "player_2_points"]]
-y_train = train.player_1_wins
-X_test = test[["player_1_points", "player_2_points"]]
-y_test = test.player_1_wins
+X_train, X_test, y_train, y_test= train_test_split(X, y, test_size = 0.2, shuffle=False)
 
 
-my_model = RandomForestClassifier(n_estimators=100, random_state=0)
-my_model.fit(X_train, y_train)
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+categorical_cols = [cname for cname in X_train.columns if
+                    X_train[cname].nunique() < 10 and 
+                    X_train[cname].dtype == "object"]
 
-# Predict
-y_pred = pd.Series(classifier.predict(X_test), index = y_test.index)
-mae = mean_absolute_error(y_pred, y_test)
-print("MAE using option 2: {}".format(mae))
+# Select numerical columns
+numerical_cols = [cname for cname in X_train.columns if 
+                X_train[cname].dtype in ['int64', 'float64']]
 
+# Keep selected columns only
+my_cols = categorical_cols + numerical_cols
+X_train = X_train[my_cols].copy()
+X_test = X_test[my_cols].copy()
+
+# Preprocessing for numerical data 
+from sklearn.preprocessing import StandardScaler
+numerical_transformer = Pipeline(steps=[
+    ('simple', SimpleImputer(strategy='mean')), # Fill missing values
+    ('normalizer', StandardScaler()), # Normalize data        
+])
+
+# Preprocessing for categorical data
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),   #Fill missing values
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))      #Create 1 column per value
+])
+
+# Bundle preprocessing for numerical and categorical data
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ])
+
+# Define model
+my_model = LogisticRegression()
+
+# Bundle preprocessing and modeling code in a pipeline
+my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                      ('model', my_model)
+                     ])
+
+
+# Preprocessing of training data, fit model 
+my_pipeline.fit(X_train, y_train)
+
+# Preprocessing of validation data, get predictions
+y_pred = my_pipeline.predict(X_test)
+
+accuracy = sum(y_pred == y_test.to_numpy()) / len(y_pred)
+
+print(accuracy)
 
 # Ideas : Nationality
 #  matchups, "winner_id", "loser_id", 
-
-
-'''
-criteria1 = dataset["winner_id"] == 104755
-criteria2 = dataset["loser_id"] == 104755
-test = dataset[(criteria1 | criteria2 )]
-
-unique = np.unique(np.concatenate([dataset["winner_id"].unique(), dataset["loser_id"].unique()]))
-'''
-
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-
-columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop="first"), ["surface"])], remainder='passthrough')
-#remainder='passthrough' : keep other columns (default:'drop')
-
-dataset = np.array(columnTransformer.fit_transform(dataset))
