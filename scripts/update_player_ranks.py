cb3751da079af2ab0ca3b0c2170d25cf2838b6b7
davy-datascience <davy.datascience@gmail.com>
2021-03-05 00:03:25 +0000

    script update_player_ranks.py ready

--- scripts/update_player_ranks.py
+++ scripts/update_player_ranks.py
@@ -1,11 +1,7 @@
-from datetime import datetime
 import sys
-import os
 
 PROJECT_FOLDER = '/home/davy/Documents/tennis-prediction/'
 LOG_FILENAME = '{0}logs/update_player_ranks.out'.format(PROJECT_FOLDER)
-
-myFile = open(LOG_FILENAME, 'a')
 
 try:
     sys.path.append("/home/davy/Documents/tennis-prediction/")
@@ -13,10 +9,8 @@ try:
     from managers.player_rank_manager import scrap_all_player_ranks
     from log import log_to_file
 
-    scrap_all_player_ranks()
-    log_to_file("OK", LOG_FILENAME)
-
+    scrap_all_player_ranks(LOG_FILENAME)
 
 except Exception as ex:
+    myFile = open(LOG_FILENAME, 'a')
     myFile.write("\n" + str(ex))
-
